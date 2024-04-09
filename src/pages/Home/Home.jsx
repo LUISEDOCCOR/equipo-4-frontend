3fda46b4b9debcae1308b99ceb1013bcb3da2be1
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-04-09 11:54:49 -0600

    feat: create the home and login page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+    return(
+        <main className="flex justify-center items-center h-screen">
+            <h1 className="text-5xl font-semibold">Gestor de contraseñas</h1>
+        </main>
+    )
+}
