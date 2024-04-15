fe62b4486bc61cd91ee2c7ec3298bf971bf09be4
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-04-14 18:08:23 -0600

    feat: add design colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				cOrange: "#ff6464",
+				cGrey: "#696d85"
+			}
+		},
 	},
 	plugins: [require("daisyui")],
 	themes: ["light"],
