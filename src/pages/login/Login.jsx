7e6256d91dd483be9321a9ccada2ea743a3f4e53
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-04-14 19:41:11 -0600

    feat: create login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,56 @@
 export const Login = () => {
     return(
-        <main>
-            <h1 className="text-cOrange">Login</h1>
+        <>
+        <header className="mx-auto max-w-6xl mt-20">
+            <span className="text-3xl text-cOrange font-bold">***</span>
+            <h1 className="text-cGrey uppercase font-bold text-6xl mt-6">
+                login
+            </h1>
+            <h4 className="text-cLightGrey text-xl mt-4">
+                Let’s get you setup with a new account!
+            </h4>
+        </header>
+        <main className="mx-auto max-w-6xl mt-24">
+            <form action="" className="space-y-6 w-3/4">
+                <label className="form-control w-full">
+                    <div className="label">
+                        <span className="label-text text-cGrey font-bold uppercase">EMAIL</span>
+                    </div>
+                    <input 
+                        type="text" 
+                        placeholder="Type here" 
+                        className="input input-bordered w-full" 
+                    />
+                </label>
+                <label className="form-control w-full">
+                    <div className="label">
+                        <span className="label-text text-cGrey font-bold uppercase">PASSWORD</span>
+                    </div>
+                    <input 
+                        type="password" 
+                        placeholder="Type here" 
+                        className="input input-bordered w-full" 
+                    />
+                </label>
+                <button 
+                    className="btn bg-cOrange w-full text-white uppercase"
+                    type="submit"
+                >
+                    login
+                </button>
+            </form>
+            <article className="mt-8 w-3/4 text-center">
+                <a href="" className="text-cOrange uppercase">
+                    Forgot Password
+                </a>
+                <section className="mt-6">
+                    <p>Don’t have an account yet?</p>
+                    <a href="" className="uppercase text-cOrange font-semibold">
+                        REGISTER
+                    </a>
+                </section>
+            </article>
         </main>
+        </> 
     )
 }
